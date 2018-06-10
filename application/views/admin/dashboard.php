cf8d924cc045a143a5d9813171cda125b7881e7c
William Mucheru <willyk99@gmail.com>
2018-06-10 22:06:08 +0300

    Dashboard apge

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -1,3 +1,4 @@
+<div class="container">
     <div class="page-header">
         <h1><?php echo isset($page_title) ? $page_title : ''; ?></h1>
         <div class="top-navigation">
@@ -8,38 +9,6 @@
         </div>
     </div>
     <div class="page-content clearfix">
-        <div class="row">
-            <div class="col-sm-6">
-                <h4 style="margin-top:0;">Recent Events</h4>
-                <div class="row">
-                <?php
-                    # var_dump($events);
-
-                    if(!empty($events) && count($events) > 0){
-
-                        foreach($events as $event){
-                ?>
-                <div class="clearfix event-img col-sm-4 event-box">
-                    <a href="<?php echo site_url('admin/events/view/' . $event->id); ?>" class="clearfix">
-                        <img 
-                            src="<?php echo base_url('content/uploads/posters/' . $event->poster); ?>"
-                            class="img-responsive"
-                            alt="" />
-                        <h5 class="event-title" title="<?php echo $event->name; ?>"><?php echo $event->name; ?></h5>
-                    </a>
-                </div>
-
-                <?php
-                        } # ENDFOREACH: Loop through events
-                    }
-                    else{
-                        echo '<div class="alert alert-warning">No recent events found</div>';
-                    }
-                ?>
-                </div>
-            </div>
-            <div class="col-sm-6 event-calendar">
-                <div id="calendar"></div>
-            </div>
-        </div>
+        <h4 style="margin-top:0;">All Data</h4>
     </div>
+</div>
