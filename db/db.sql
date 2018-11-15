8eca7de29f7a61bd1a9daca59be54ebb51d37ba8
William Mucheru <willyk99@gmail.com>
2018-11-15 16:39:08 +0300

    Update db

--- db/db.sql
+++ db/db.sql
@@ -1,178 +1,282 @@
-/*
-	Aauth SQL Table Structure
-*/
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Nov 15, 2018 at 02:38 PM
+-- Server version: 5.6.35
+-- PHP Version: 7.0.15
 
-SET FOREIGN_KEY_CHECKS=0;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
--- ----------------------------
--- Table structure for `aauth_groups`
--- ----------------------------
-DROP TABLE IF EXISTS `aauth_groups`;
+--
+-- Database: `ci_admin`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `aauth_groups`
+--
+
 CREATE TABLE `aauth_groups` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(100),
-  `definition` text,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+  `id` int(11) UNSIGNED NOT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `definition` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- ----------------------------
--- Records of aauth_groups
--- ----------------------------
-INSERT INTO `aauth_groups` VALUES ('1', 'Admin', 'Super Admin Group');
-INSERT INTO `aauth_groups` VALUES ('2', 'Public', 'Public Access Group');
-INSERT INTO `aauth_groups` VALUES ('3', 'Default', 'Default Access Group');
+--
+-- Dumping data for table `aauth_groups`
+--
 
--- ----------------------------
--- Table structure for `aauth_perms`
--- ----------------------------
-DROP TABLE IF EXISTS `aauth_perms`;
+INSERT INTO `aauth_groups` (`id`, `name`, `definition`) VALUES
+(1, 'Admin', 'Super Admin Group'),
+(2, 'Public', 'Public Access Group'),
+(3, 'Default', 'Default Access Group');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `aauth_group_to_group`
+--
+
+CREATE TABLE `aauth_group_to_group` (
+  `group_id` int(11) UNSIGNED NOT NULL,
+  `subgroup_id` int(11) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `aauth_login_attempts`
+--
+
+CREATE TABLE `aauth_login_attempts` (
+  `id` int(11) NOT NULL,
+  `ip_address` varchar(39) DEFAULT '0',
+  `timestamp` datetime DEFAULT NULL,
+  `login_attempts` tinyint(2) DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `aauth_login_attempts`
+--
+
+INSERT INTO `aauth_login_attempts` (`id`, `ip_address`, `timestamp`, `login_attempts`) VALUES
+(4, '::1', '2018-11-15 16:01:42', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `aauth_perms`
+--
+
 CREATE TABLE `aauth_perms` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(100),
-  `definition` text,
-  PRIMARY KEY (`id`)
+  `id` int(11) UNSIGNED NOT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `definition` text
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- ----------------------------
--- Records of aauth_perms
--- ----------------------------
+-- --------------------------------------------------------
 
--- ----------------------------
--- Table structure for `aauth_perm_to_group`
--- ----------------------------
-DROP TABLE IF EXISTS `aauth_perm_to_group`;
+--
+-- Table structure for table `aauth_perm_to_group`
+--
+
 CREATE TABLE `aauth_perm_to_group` (
-  `perm_id` int(11) unsigned NOT NULL,
-  `group_id` int(11) unsigned NOT NULL,
-  PRIMARY KEY (`perm_id`,`group_id`)
+  `perm_id` int(11) UNSIGNED NOT NULL,
+  `group_id` int(11) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- ----------------------------
--- Records of aauth_perm_to_group
--- ----------------------------
+-- --------------------------------------------------------
 
--- ----------------------------
--- Table structure for `aauth_perm_to_user`
--- ----------------------------
-DROP TABLE IF EXISTS `aauth_perm_to_user`;
+--
+-- Table structure for table `aauth_perm_to_user`
+--
+
 CREATE TABLE `aauth_perm_to_user` (
-  `perm_id` int(11) unsigned NOT NULL,
-  `user_id` int(11) unsigned NOT NULL,
-  PRIMARY KEY (`perm_id`,`user_id`)
+  `perm_id` int(11) UNSIGNED NOT NULL,
+  `user_id` int(11) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- ----------------------------
--- Records of aauth_perm_to_user
--- ----------------------------
+-- --------------------------------------------------------
 
--- ----------------------------
--- Table structure for `aauth_pms`
--- ----------------------------
-DROP TABLE IF EXISTS `aauth_pms`;
+--
+-- Table structure for table `aauth_pms`
+--
+
 CREATE TABLE `aauth_pms` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `sender_id` int(11) unsigned NOT NULL,
-  `receiver_id` int(11) unsigned NOT NULL,
+  `id` int(11) UNSIGNED NOT NULL,
+  `sender_id` int(11) UNSIGNED NOT NULL,
+  `receiver_id` int(11) UNSIGNED NOT NULL,
   `title` varchar(255) NOT NULL,
   `message` text,
   `date_sent` datetime DEFAULT NULL,
   `date_read` datetime DEFAULT NULL,
   `pm_deleted_sender` int(1) DEFAULT NULL,
-  `pm_deleted_receiver` int(1) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `full_index` (`id`,`sender_id`,`receiver_id`,`date_read`)
+  `pm_deleted_receiver` int(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- ----------------------------
--- Records of aauth_pms
--- ----------------------------
+-- --------------------------------------------------------
 
--- ----------------------------
--- Table structure for `aauth_users`
--- ----------------------------
-DROP TABLE IF EXISTS `aauth_users`;
+--
+-- Table structure for table `aauth_users`
+--
+
 CREATE TABLE `aauth_users` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `email` varchar(100) COLLATE utf8_general_ci NOT NULL,
-  `pass` varchar(64) COLLATE utf8_general_ci NOT NULL,
-  `username` varchar(100) COLLATE utf8_general_ci,
+  `id` int(11) UNSIGNED NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `pass` varchar(64) NOT NULL,
+  `username` varchar(100) DEFAULT NULL,
   `banned` tinyint(1) DEFAULT '0',
   `last_login` datetime DEFAULT NULL,
   `last_activity` datetime DEFAULT NULL,
   `date_created` datetime DEFAULT NULL,
-  `forgot_exp` text COLLATE utf8_general_ci,
+  `forgot_exp` text,
   `remember_time` datetime DEFAULT NULL,
-  `remember_exp` text COLLATE utf8_general_ci,
-  `verification_code` text COLLATE utf8_general_ci,
-  `totp_secret` varchar(16) COLLATE utf8_general_ci DEFAULT NULL,
-  `ip_address` text COLLATE utf8_general_ci,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+  `remember_exp` text,
+  `verification_code` text,
+  `totp_secret` varchar(16) DEFAULT NULL,
+  `ip_address` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- ----------------------------
--- Records of aauth_users
--- ----------------------------
-INSERT INTO `aauth_users` VALUES ('1', 'admin@example.com', 'dd5073c93fb477a167fd69072e95455834acd93df8fed41a2c468c45b394bfe3', 'Admin', '0', null, null, null, null, null, null, null, null, '0');
+--
+-- Dumping data for table `aauth_users`
+--
 
--- ----------------------------
--- Table structure for `aauth_user_to_group`
--- ----------------------------
-DROP TABLE IF EXISTS `aauth_user_to_group`;
+INSERT INTO `aauth_users` (`id`, `email`, `pass`, `username`, `banned`, `last_login`, `last_activity`, `date_created`, `forgot_exp`, `remember_time`, `remember_exp`, `verification_code`, `totp_secret`, `ip_address`) VALUES
+(1, 'admin@example.com', '5711aa2253ac62088bf34f79f8ccd82e41bdbcf32e7670772d2a1e1746a9be9b', 'admin', 0, '2018-11-15 16:16:53', '2018-11-15 16:16:53', '2018-11-15 16:15:30', NULL, '2018-12-15 00:00:00', 'X3oyOUV5TmvSZMIQ', NULL, NULL, '::1');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `aauth_user_to_group`
+--
+
 CREATE TABLE `aauth_user_to_group` (
-  `user_id` int(11) unsigned NOT NULL,
-  `group_id` int(11) unsigned NOT NULL,
-  PRIMARY KEY (`user_id`,`group_id`)
+  `user_id` int(11) UNSIGNED NOT NULL,
+  `group_id` int(11) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- ----------------------------
--- Records of aauth_user_to_group
--- ----------------------------
-INSERT INTO `aauth_user_to_group` VALUES ('1', '1');
-INSERT INTO `aauth_user_to_group` VALUES ('1', '3');
+--
+-- Dumping data for table `aauth_user_to_group`
+--
 
--- ----------------------------
--- Table structure for `aauth_user_variables`
--- ----------------------------
-DROP TABLE IF EXISTS `aauth_user_variables`;
+INSERT INTO `aauth_user_to_group` (`user_id`, `group_id`) VALUES
+(1, 1),
+(1, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `aauth_user_variables`
+--
+
 CREATE TABLE `aauth_user_variables` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) unsigned NOT NULL,
+  `id` int(11) UNSIGNED NOT NULL,
+  `user_id` int(11) UNSIGNED NOT NULL,
   `data_key` varchar(100) NOT NULL,
-  `value` text,
-  PRIMARY KEY (`id`),
-  KEY `user_id_index` (`user_id`)
+  `value` text
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- ----------------------------
--- Records of aauth_user_variables
--- ----------------------------
+--
+-- Indexes for dumped tables
+--
 
--- ----------------------------
--- Table structure for `aauth_group_to_group`
--- ----------------------------
-DROP TABLE IF EXISTS `aauth_group_to_group`;
-CREATE TABLE `aauth_group_to_group` (
-  `group_id` int(11) unsigned NOT NULL,
-  `subgroup_id` int(11) unsigned NOT NULL,
-  PRIMARY KEY (`group_id`,`subgroup_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+--
+-- Indexes for table `aauth_groups`
+--
+ALTER TABLE `aauth_groups`
+  ADD PRIMARY KEY (`id`);
 
--- ----------------------------
--- Records of aauth_group_to_group
--- ----------------------------
+--
+-- Indexes for table `aauth_group_to_group`
+--
+ALTER TABLE `aauth_group_to_group`
+  ADD PRIMARY KEY (`group_id`,`subgroup_id`);
 
--- ----------------------------
--- Table structure for `aauth_login_attempts`
--- ----------------------------
+--
+-- Indexes for table `aauth_login_attempts`
+--
+ALTER TABLE `aauth_login_attempts`
+  ADD PRIMARY KEY (`id`);
 
-CREATE TABLE IF NOT EXISTS `aauth_login_attempts` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `ip_address` varchar(39) DEFAULT '0',
-  `timestamp` datetime DEFAULT NULL,
-  `login_attempts` tinyint(2) DEFAULT '0',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+--
+-- Indexes for table `aauth_perms`
+--
+ALTER TABLE `aauth_perms`
+  ADD PRIMARY KEY (`id`);
 
--- ----------------------------
--- Records of aauth_login_attempts
--- ----------------------------
+--
+-- Indexes for table `aauth_perm_to_group`
+--
+ALTER TABLE `aauth_perm_to_group`
+  ADD PRIMARY KEY (`perm_id`,`group_id`);
+
+--
+-- Indexes for table `aauth_perm_to_user`
+--
+ALTER TABLE `aauth_perm_to_user`
+  ADD PRIMARY KEY (`perm_id`,`user_id`);
+
+--
+-- Indexes for table `aauth_pms`
+--
+ALTER TABLE `aauth_pms`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `full_index` (`id`,`sender_id`,`receiver_id`,`date_read`);
+
+--
+-- Indexes for table `aauth_users`
+--
+ALTER TABLE `aauth_users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `aauth_user_to_group`
+--
+ALTER TABLE `aauth_user_to_group`
+  ADD PRIMARY KEY (`user_id`,`group_id`);
+
+--
+-- Indexes for table `aauth_user_variables`
+--
+ALTER TABLE `aauth_user_variables`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id_index` (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `aauth_groups`
+--
+ALTER TABLE `aauth_groups`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `aauth_login_attempts`
+--
+ALTER TABLE `aauth_login_attempts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT for table `aauth_perms`
+--
+ALTER TABLE `aauth_perms`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `aauth_pms`
+--
+ALTER TABLE `aauth_pms`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `aauth_users`
+--
+ALTER TABLE `aauth_users`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `aauth_user_variables`
+--
+ALTER TABLE `aauth_user_variables`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
