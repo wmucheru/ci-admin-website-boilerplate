bc765c141ad2b923e43226dd43a9c4f65b4ce2a1
William Mucheru <willyk99@gmail.com>
2019-11-30 19:08:39 +0300

    Render admin page

--- application/helpers/render_helper.php
+++ application/helpers/render_helper.php
@@ -27,4 +27,25 @@ if(!function_exists('render_page')){
     }
 }
 
+if(!function_exists('render_admin')){
+
+    function render_admin($pageContent, $pageTitle='', $bodyClass='', $pageData=array()){
+        $CI =& get_instance();
+
+        $data['page_title'] = $pageTitle;
+        $data['page_content'] = $pageContent;
+        $data['body_class'] = $bodyClass;
+
+        if(!empty($pageData)){
+            
+            // Add keys to $data object
+            foreach(array_keys($pageData) as $d){
+                $data[$d] = $pageData[$d];
+            }
+        }
+        
+        $CI->load->view('inc/template-admin', $data);
+    }
+}
+
 ?>
