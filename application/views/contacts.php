ba117039f2bef12875399594c05dd9c1e1921396
William Mucheru <willyk99@gmail.com>
2019-12-15 19:19:05 +0300

    Update contact template; add map

--- application/views/contacts.php
+++ application/views/contacts.php
@@ -3,11 +3,38 @@
 		<h1>Contact Us</h1>
 	</div>
 
-	<p>
-		For more information or any queries that you may have:
-	</p>
-	<ul class="infolist">
-		<li>Email us: <a href="mailto:abc@xyz.site">abc@xyz.site</a></li>
-		<li>Call us: <a href="tel:+000 000 000">+000 000 000</a></li>
-	</ul>
+	<div class="clearfix">
+        <div class="row">
+            <div class="col-sm-6">
+                <style>
+                    iframe{
+                        background:#eeee;
+                        border-collapse:collapse;
+                        height:400px;
+                        width:100%;
+                        margin:0;
+                    }
+                    .mapouter {
+                        position:relative;
+                        text-align:right;
+                    }
+                </style>
+                <div class="mapouter">
+                    <iframe frameborder="0"
+                        src="https://maps.google.com/maps?q=nairobi&t=&z=15&ie=UTF8&iwloc=&output=embed"></iframe>
+                    <a href="https://www.emojilib.com"></a>
+                </div>
+            </div>
+
+            <div class="col-sm-6">
+                <p>
+                    For more information or any queries that you may have:
+                </p>
+                <ul class="infolist">
+                    <li>Email us: <a href="mailto:abc@xyz.site">abc@xyz.site</a></li>
+                    <li>Call us: <a href="tel:+000 000 000">+000 000 000</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
 </div>
