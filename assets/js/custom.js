790cef296ef6ab36aead38ee795b22212ed9c5fe
William Mucheru <willyk99@gmail.com>
2020-12-30 15:40:49 +0300

    Update scripts

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,21 +1,75 @@
-$(document).ready(function(){
+$(document).ready(function () {
+    var dt = $('.dt'),
+        datePicker = $('.datepicker'),
+        homeSlider = $('.home-slider');
 
-    $('.no-enter input, .no-enter textarea').keydown(function(e){
-        if(e.keyCode == 13){
+    if (dt.length > 0) {
+        $('.dt').dataTable({
+            bSort: false, // Prevent autosort
+            dom: 'Bfrtip',
+            //lengthChange: false,
+            lengthMenu: [
+                [10, 25, 50, 75, 100, -1],
+                ['10 rows', '25 rows', '50 rows', '75 rows', '100 rows', 'Show all']
+            ],
+            buttons: [
+                'pageLength',
+                {
+                    extend: 'copyHtml5',
+                    text: 'Copy',
+                    titleAttr: 'Copy',
+                    exportOptions: {
+                        columns: [0, ':visible']
+                    }
+                }, {
+                    extend: 'excelHtml5',
+                    text: 'Excel',
+                    titleAttr: 'Excel',
+                    exportOptions: {
+                        columns: ':visible'
+                    }
+                }, {
+                    extend: 'pdfHtml5',
+                    text: 'PDF',
+                    titleAttr: 'PDF',
+                    exportOptions: {
+                        columns: ':visible'
+                    }
+                }/*,{
+                    extend: 'colvis',
+                    className: 'hidden-print',
+                    text: '<i class="fa fa-columns"></i>',
+                    titleAttr: 'Hide Columns'
+                }*/
+            ]
+        })
+    }
+
+    $('.no-enter input, .no-enter textarea').keydown(function (e) {
+        if (e.keyCode == 13) {
             e.preventDefault()
             return false
         }
     })
 
     /* Password toggle */
-    $('#password-toggle').change(function(e){
+    $('#password-toggle').change(function (e) {
         var target = document.getElementById('signup-pwd')
         target.type = target.type === "password"
             ? "text"
             : "password"
     })
 
-    const homeSlider = $('.home-slider')
+    /*  Datepicker  */
+    if (datePicker.length > 0) {
+        datePicker.datepicker({
+            format: 'yyyy-mm-dd',
+            // startDate: '-0d',
+            setDate: new Date()
+        }).on('changeDate', function (e) {
+            $('.datepicker.dropdown-menu').hide()
+        })
+    }
 
     if (homeSlider.length > 0) {
         homeSlider.slick({
