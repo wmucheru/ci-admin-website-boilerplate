7c1b6b6031248a517306e35308b89eec84f6b0cf
William Mucheru <willyk99@gmail.com>
2021-03-05 13:54:53 +0300

    Datatable styles

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -1,12 +1,10 @@
 <div class="clearfix">
     <div class="page-header">
         <h1><?php echo isset($page_title) ? $page_title : ''; ?></h1>
-        <div class="top-navigation">
-            <ol class="breadcrumb">
-                <li><a href="<?php echo site_url('admin/dashboard'); ?>">Home</a></li>
-                <li class="active"><?php echo isset($page_title) ? $page_title : ''; ?></li>
-            </ol>
-        </div>
+        <ol class="breadcrumb">
+            <li><a href="<?php echo site_url('admin/dashboard'); ?>">Home</a></li>
+            <li class="active"><?php echo isset($page_title) ? $page_title : ''; ?></li>
+        </ol>
     </div>
 
     <div class="page-content clearfix">
@@ -14,7 +12,7 @@
             <?php
                 # var_dump($stats);
 
-                stat_box($stats->users, 'Properties');
+                stat_box($stats->users, 'Users');
             ?>
         </div>
     </div>
